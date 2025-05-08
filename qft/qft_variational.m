05c5fae4ab9c86f2af45fd9200568da919ae9e00
mike-dixon <dixon@ucar.edu>
2025-05-08 17:28:36 -0600

    Preparing for processing

--- qft/qft_variational.m
+++ qft/qft_variational.m
@@ -1,18 +1,29 @@
 clear all
 close all
 
-figdir='/scr/virga1/rsfdata/projects/qft/';
+tic
+
+%figdir='/scr/virga1/rsfdata/projects/qft/d5_deltaVaries_lambda1/';
+figdir='/scr/virga1/rsfdata/projects/qft/dVaries_delta1overD_lambda1/';
 
 % Initialize
-d=6;
+d=8;
 nMax=10;
 
 w=1:0.25:10;
 lambda=1;
-delta=1/5;
-m2=-(fliplr(0:2:20));
+delta=1/d;
+%delta=0.05:0.05:1;
+%m2=-(fliplr(0:2:20));
+m2=-(fliplr(12:1:16));
 
-cutoffAll=100:100:1000;
+%cutoffAll=100:100:1000;
+%cutoffAll=[100,500,1000];
+cutoffAll=[100,1500];
+
+if ~exist(figdir,'dir')
+    mkdir(figdir)
+end
 
 %% Create pregens
 
@@ -47,82 +58,135 @@ for ii=1:size(sC,1)
     end
 end
 
-close all
-f1 = figure('Position',[200 500 600 600],'DefaultAxesFontSize',12);
+%% Loop over delta
 
-t = tiledlayout(1,1,'TileSpacing','tight','Padding','tight');
-s1=nexttile(1);
-colmap=turbo(length(cutoffAll));
+for ll=1:length(delta)
 
-hold on
+    close all
+    f1 = figure('Position',[200 500 600 600],'DefaultAxesFontSize',12);
 
-%% Loop over cutoff
+    t = tiledlayout(1,1,'TileSpacing','tight','Padding','tight');
+    s1=nexttile(1);
+    colmap=turbo(length(cutoffAll));
 
-bc=nan(length(cutoffAll),length(m2));
+    hold on
 
-for kk=1:length(cutoffAll)
-    cutoff=cutoffAll(kk);
+    %% Loop over cutoff
 
-    disp(['Cutoff: ',num2str(cutoff)]);
+    bcE=nan(length(cutoffAll),length(m2));
+    bcO=nan(length(cutoffAll),length(m2));
 
-    minAllE=nan(1,length(m2));
-    minAllEw=nan(1,length(m2));
-    vecAllE=nan(cutoff,length(m2));
-    x2exE=nan(1,length(m2));
-    x4exE=nan(1,length(m2));
+    for kk=1:length(cutoffAll)
+        cutoff=cutoffAll(kk);
 
-    [matOutE1,matOutE2,matOutE3]=calcMatrix(evensS,d,w',delta,cutoff);
+        disp(['Cutoff: ',num2str(cutoff)]);
 
-    for jj=1:length(m2)
+        minAllE=nan(1,length(m2));
+        minAllEw=nan(1,length(m2));
+        vecAllE=nan(cutoff,length(m2));
+        x2exE=nan(1,length(m2));
+        x4exE=nan(1,length(m2));
 
-        disp(['Loop over m: ',num2str(jj),' of ',num2str(length(m2))]);
+        minAllO=nan(1,length(m2));
+        minAllOw=nan(1,length(m2));
+        vecAllO=nan(cutoff,length(m2));
+        x2exO=nan(1,length(m2));
+        x4exO=nan(1,length(m2));
 
-        % Evens
-        
-        matOutE=matOutE1+matOutE2.*(m2(jj)+2./delta.^2)./2+matOutE3.*lambda./delta;
-       
-        % Eigen values
-        minE=nan(1,length(w));
-        vE=nan(cutoff,length(w));
-        
-        for ii=1:length(w)
-            [V,D]=eig(matOutE(:,:,ii));
-            ee=(D(sub2ind(size(D),1:size(D,1),1:size(D,2))))';
-            [minE(ii),minEind]=min(ee);
-            vE(:,ii)=V(:,minEind);
+        disp('Even matrix');
+        [matOutE1,matOutE2,matOutE3]=calcMatrix(evensS,d,w',delta(ll),cutoff);
+        disp('Odd matrix');
+        [matOutO1,matOutO2,matOutO3]=calcMatrix(oddsS,d,w',delta(ll),cutoff);
+
+        for jj=1:length(m2)
+
+            disp(['Loop over m: ',num2str(jj),' of ',num2str(length(m2))]);
+
+            % Evens
+
+            matOutE=matOutE1+matOutE2.*(m2(jj)+2./delta(ll).^2)./2+matOutE3.*lambda./delta(ll);
+
+            % Eigen values
+            minE=nan(1,length(w));
+            vE=nan(cutoff,length(w));
+
+            for ii=1:length(w)
+                [V,D]=eig(matOutE(:,:,ii));
+                ee=(D(sub2ind(size(D),1:size(D,1),1:size(D,2))))';
+                [minE(ii),minEind]=min(ee);
+                vE(:,ii)=V(:,minEind);
+            end
+
+            [minAllE(jj),minAllEind]=min(minE);
+            minAllEw(jj)=w(minAllEind);
+            vecAllE(:,jj)=vE(:,minAllEind);
+
+            matThisE2=matOutE2(:,:,minAllEind);
+            x2exE(jj)=vecAllE(:,jj)'*matThisE2*vecAllE(:,jj);
+            matThisE3=matOutE3(:,:,minAllEind);
+            x4exE(jj)=vecAllE(:,jj)'*matThisE3*vecAllE(:,jj).*d;
+
+            % Odds
+
+            matOutO=matOutO1+matOutO2.*(m2(jj)+2./delta(ll).^2)./2+matOutO3.*lambda./delta(ll);
+
+            % Eigen values
+            minO=nan(1,length(w));
+            vO=nan(cutoff,length(w));
+
+            for ii=1:length(w)
+                [V,D]=eig(matOutO(:,:,ii));
+                eo=(D(sub2ind(size(D),1:size(D,1),1:size(D,2))))';
+                [minO(ii),minOind]=min(eo);
+                vO(:,ii)=V(:,minOind);
+            end
+
+            [minAllO(jj),minAllOind]=min(minO);
+            minAllOw(jj)=w(minAllOind);
+            vecAllO(:,jj)=vO(:,minAllOind);
+
+            matThisO2=matOutO2(:,:,minAllOind);
+            x2exO(jj)=vecAllO(:,jj)'*matThisO2*vecAllO(:,jj);
+            matThisO3=matOutO3(:,:,minAllOind);
+            x4exO(jj)=vecAllO(:,jj)'*matThisO3*vecAllO(:,jj).*d;
+
         end
 
-        [minAllE(jj),minAllEind]=min(minE);
-        minAllEw(jj)=w(minAllEind);
-        vecAllE(:,jj)=vE(:,minAllEind);
+        bcE(kk,:)=1-x4exE./x2exE.^2./(3.*delta(ll).*d);
+        bcO(kk,:)=1-x4exO./x2exO.^2./(3.*delta(ll).*d);
 
-        matThisE2=matOutE2(:,:,minAllEind);
-        x2exE(jj)=vecAllE(:,jj)'*matThisE2*vecAllE(:,jj)/2;
-        matThisE3=matOutE3(:,:,minAllEind);
-        x4exE(jj)=vecAllE(:,jj)'*matThisE3*vecAllE(:,jj);
+        plot(m2,bcE(kk,:),'-','LineWidth',2,'Color',colmap(kk,:));
+        plot(m2,bcO(kk,:),'--','LineWidth',2,'Color',colmap(kk,:));
+        drawnow
 
+        outTable=table(m2',minAllEw',minAllE',x2exE',x4exE',bcE',minAllOw',minAllO',x2exO',x4exO',bcO', ...
+            'VariableNames',{'m2','even_w_min','E0','<x2>_even','<x4>_even','B_even', ...
+            'odd_w_min','E1','<x2>_odd','<x4>_odd','B_odd'});
+        writetable(outTable,[figdir,'mpx_d',num2str(d),'_delta',num2str(delta(ll)),'_lambda',num2str(lambda),'_cutoff',num2str(cutoff),'.txt'],'Delimiter','\t');
     end
 
-    bc(kk,:)=1-x4exE./x2exE.^2./3;
-   
-    plot(m2,bc(kk,:),'-','LineWidth',2,'Color',colmap(kk,:));
-    drawnow
+    %% Finish plot
+    xlabel('m2');
+    ylabel('bc');
+
+    xlim([m2(1),m2(end)]);
+
+    cutoffLeg=repmat(cutoffAll,2,1);
+    cutoffLeg=cutoffLeg(:);
+    legend(string(cutoffLeg));
+
+    title(['d: ',num2str(d),', delta: ',num2str(delta(ll)),', lambda: ',num2str(lambda)]);
+
+    grid on
+    box on
+
+    print([figdir,'mpxFig_d',num2str(d),'_delta',num2str(delta(ll)),'_lambda',num2str(lambda),'.png'],'-dpng','-r0');
+
 end
 
-%% Finish plot
-xlabel('m2');
-ylabel('bc');
+elapsedTime=toc;
 
-xlim([m2(1),m2(end)]);
-
-legend(string(cutoffAll));
-
-grid on
-box on
-
-print([figdir,'qft_variations_bc_d',num2str(d),'.png'],'-dpng','-r0');
-
-save([figdir,'qft_variations_bc_d',num2str(d),'.mat'],'bc','m2');
+disp(['Processing took ',num2str(elapsedTime/60/60,2),' hours.']);
 
 %% Stop parallel pool
 
